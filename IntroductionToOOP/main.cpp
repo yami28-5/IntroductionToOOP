6f447f42a2aa70530b42691730482717518d05a2
Богдан пензин <bogdanpenzin432@gmail.com>
2025-10-08 15:25:36 +0700

    distance DONE

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -2,10 +2,73 @@
 
 using namespace std;
 
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+	    this->y = y;
+	}
+
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+};
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
+
+//#define STRUCT_POINT
+
 void main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef STRUCT_POINT
 	cout << "Hello OOP" << endl;
+	Point A;
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
 
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
 
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+	cout << "Расстояние от точки 'А' до точки 'В': " << A.distance(B) << endl;
+	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
+	cout << "Расстояние между точками 'B' и 'A': " << distance(B, A) << endl;
 }
